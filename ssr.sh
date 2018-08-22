49e6399d8a29a11f173a6d22e30260b2dbac7cf5
Sakura <40120664+HyoukaChitandaEru@users.noreply.github.com>
2018-08-22 08:07:26 +0800

    Update ssr.sh

--- ssr.sh
+++ ssr.sh
@@ -606,7 +606,7 @@ Debian_apt(){
 # 下载 ShadowsocksR
 Download_SSR(){
 	cd "/usr/local/"
-	wget -N --no-check-certificate "https://github.com/HyoukaChitandaEru/SSR/raw/master/shadowsocksr-manyuser.zip"
+	wget -N --no-check-certificate "https://github.com/HyoukaChitandaEru/shadowsocksr/archive/master.zip"
 	#git config --global http.sslVerify false
 	#env GIT_SSL_NO_VERIFY=true git clone -b manyuser https://github.com/HyoukaChitandaEru/SSR
 	#[[ ! -e ${ssr_folder} ]] && echo -e "${Error} ShadowsocksR服务端 下载失败 !" && exit 1
